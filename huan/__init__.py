3b44a6373e44b3905a86b47315d40f3b08b08e23
akino <6130092+cycleuser@users.noreply.github.com>
2026-03-09 17:23:50 +0800

    chore: update and upload Huan package

--- huan/__init__.py
+++ huan/__init__.py
@@ -5,7 +5,7 @@ A command-line tool that converts web pages to Markdown files,
 preserving the site's URL structure as a local folder hierarchy.
 """
 
-__version__ = "0.2.2"
+__version__ = "0.2.3"
 __author__ = "huan contributors"
 
 from .core import SiteCrawler, main
